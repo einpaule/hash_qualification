ee084dee5002182739f875b294ca2f112e52acf8
Michele Delle Vergini <michele.dellevergini@wattics.com>
2016-02-11 18:20:02 +0000

    Add Product class

---
+++ qualification/src/main/java/whoop/whoop/Product.java
@@ -0,0 +1,21 @@
+package whoop.whoop;
+
+public class Product {
+	
+	protected String type;
+	protected int weight;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+}

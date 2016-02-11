b2468e89316ee63bddb0fecf2e785a850e43400d
Michele Delle Vergini <michele.dellevergini@wattics.com>
2016-02-11 21:06:55 +0000

    Add ID to Order class

--- qualification/src/main/java/whoop/whoop/Order.java
+++ qualification/src/main/java/whoop/whoop/Order.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class Order extends Destination {
 
+	public int id;
 	public List<Product> items;
 
-	public Order(int row, int column) {
+	public Order(int id, int row, int column) {
 		this.row = row;
 		this.column = column;
 	}

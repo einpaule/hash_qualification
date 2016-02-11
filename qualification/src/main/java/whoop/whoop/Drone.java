1f5cff5080416c9a7191de266f7e63d0b3ca99c8
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-02-11 19:49:15 +0100

    update Drone

--- qualification/src/main/java/whoop/whoop/Drone.java
+++ qualification/src/main/java/whoop/whoop/Drone.java
@@ -1,18 +1,23 @@
 package whoop.whoop;
 
 public class Drone {
-  private int payload;
+  public int payload;
+  public int pos_x;
+  public int pos_y;
 
   public Drone(int payload){
     this.payload = payload;
+    this.pos_x = 0;
+    this.pos_y = 0;
   }
 
-  public void setPayload(int payload) {
-    this.payload = payload;
+  public int[] getPosition() {
+    return new int[]{pos_x, pos_y};
   }
 
-  public int getPayload() {
-    return payload;
+  public void setPosition(int[] position) {
+    this.pos_x = position[0];
+    this.pos_y = position[1];
   }
 
 }

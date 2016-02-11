415f1d4f04f0d74b2c664c9adab722a8c120dbee
Paul Holthues <paul.holthues@wattics.com>
2016-02-11 21:09:29 +0000

    Added utility methods

--- qualification/src/main/java/whoop/whoop/Utils.java
+++ qualification/src/main/java/whoop/whoop/Utils.java
@@ -11,4 +11,12 @@ public class Utils {
 
     return Math.sqrt(xSqr + ySqr);
   }
+
+  static double euclideanDistance(Drone drone, Warehouse warehouse) {
+    return euclideanDistance(drone.x, drone.y, warehouse.row, warehouse.column);
+  }
+
+  static double euclideanDistance(Drone drone, Order order) {
+    return euclideanDistance(drone.x, drone.y, order.row, order.column);
+  }
 }
